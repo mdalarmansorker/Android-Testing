1f9f774e72b17c71e2315b1bec5d7003ed73004d
mdalarmansorker <mdalarmansorker@gmail.com>
2022-10-18 01:31:06 +0600

    Fun with android

--- app/src/main/java/com/mdalarmansorker/show_my_name/MainActivity.java
+++ app/src/main/java/com/mdalarmansorker/show_my_name/MainActivity.java
@@ -3,12 +3,34 @@ package com.mdalarmansorker.show_my_name;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showButton;
+    private TextView nameText;
+    private EditText enterName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+        enterName = findViewById(R.id.editTextName);
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = enterName.getText().toString();
+                if(name.isEmpty()){
+                    nameText.setText("Please Enter the name.");
+                }else {
+                    nameText.setText("Hello, " + name + "!");
+                }
+            }
+        });
     }
 }
