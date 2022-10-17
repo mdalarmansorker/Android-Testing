19b9a35079b65c9ee1a1f52d666bd7dc08cf2f99
mdalarmansorker <mdalarmansorker@gmail.com>
2022-10-17 21:51:33 +0600

    initial commit

---
+++ app/src/main/java/com/mdalarmansorker/show_my_name/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mdalarmansorker.show_my_name;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
